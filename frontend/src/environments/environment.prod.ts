2d499258f9d995df5c4bc053a200f9aea0925bb4
Gustav Olaf Yunus Laitinen-Fredriksson Lundström Imanov <olaf.laitinen@gmail.com>
2025-12-08 12:36:27 +0100

    Merge remote changes + local Brutal sync fixes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     apiUrl: '/api/v1',
+    healthUrl: '/health',
     appName: 'Promethium',
     appVersion: '1.0.0',
     pollingInterval: 10000
